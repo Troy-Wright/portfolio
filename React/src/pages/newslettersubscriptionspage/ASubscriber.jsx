dbdfdc7de1047cbe91623de7bc30ecc3a15d9063
Troy-Wright <100097237+Troy-Wright@users.noreply.github.com>
2022-07-06 11:10:34 -0700

    Create ASubscriber.jsx

---
+++ React/src/pages/newslettersubscriptionspage/ASubscriber.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+
+function ASubscriber(props) {
+    const aRecord = props.record;
+
+    return (
+        <tr>
+            <th scope="row">{aRecord.email}</th>
+            <td>{aRecord.dateCreated}</td>
+        </tr>
+    );
+}
+
+ASubscriber.propTypes = {
+    record: PropTypes.shape({
+        email: PropTypes.string,
+        dateCreated: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    }),
+};
+
+export default React.memo(ASubscriber);
